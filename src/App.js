d9bccb8d6b92f767d277964b1b10c68751afe673
Debaloy Chatterjee <debaloy110019@gmail.com>
2023-09-29 22:17:13 +0530

    api and structure updated

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-
-  // const[weatherdata, setWeatherData] = useState(null);
-
+  const[weatherdata, setWeatherData] = useState(null);
   const [location, setLocation] = useState("Mumbai");
-
-  let weatherdata = null;
-   
   const [DataIn, setDataIn] = useState(false);
 
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f4bd0d27ccf0d983a90da29ef0130a17&units=metric`;
+
   const fetchApi = async () => {
-
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f4bd0d27ccf0d983a90da29ef0130a17&units=metric`).then((data) => {
-      weatherdata = data.data;
+    axios.get(URL)
+    .then(response => {
+      setWeatherData(response.data);
       setDataIn(true);
-      console.log(weatherdata.main);
-    }).catch(err => {
+    })
+    .catch(err => {
+      setWeatherData(null);
       setDataIn(false);
-      console.log("Ram NAAM SATYA HAI");
+      console.error(err);
     });
+  };
 
-  }
-
-  useEffect( () => {
-   fetchApi();  
+  useEffect(() => {
+    fetchApi();
   },[]);
 
   return (
@@ -50,51 +47,64 @@ function App() {
 
       {
         !DataIn ?
-        (<p>No Data Found</p>)
+        <p>No Data Found</p>
         :
         (
           <div>
             <div className="location">
-              <p>{location}</p>
+              <p>
+                <span>Location: </span>
+                <strong>{location}</strong>
+              </p>
             </div>
 
 
             <div className="temp">
-              {
-                weatherdata.main != null ? <h1>{weatherdata.main.temp}^F</h1> : <p>Not Found</p>
-              }
-              <h1> </h1>
+              <p>
+                <span>Temperature: </span>
+                {
+                  weatherdata.main ? <strong>{weatherdata.main.temp}°F</strong> : "Not Found"
+                }
+              </p>
             </div>
 
 
            <div className="description">
-            {/* {
-              weatherdata.weather ?  : null
-            } */}
-            <p>{weatherdata.weather[0].main}</p>
+            <p>
+              <span>Weather: </span>
+              {
+                weatherdata.weather[0] ? <strong>{weatherdata.weather[0].main}</strong> : "N/A"
+              }
+            </p>
            </div>
 
           
            <div> 
              <div className="feels">
-              {
-                weatherdata.main ? <p className="bold">{weatherdata.main.feels_like}</p> : null
-              }
-              <p>Feels Like</p>
+              <p>
+                <span>Feels Like: </span>
+                {
+                  weatherdata.main ? <strong>{weatherdata.main.feels_like}</strong> : "N/A"
+                }
+              </p>
              </div>
 
              <div className="humidity">
-              {
-                weatherdata.main ? <p className="bold">{weatherdata.main.humidity}%</p> : null
-              }
-              <p>Humidity</p>
+              <p>
+                <span>Humidity: </span>
+                {
+                  weatherdata.main ? <strong>{weatherdata.main.humidity}%</strong> : "N/A"
+                }
+              </p>
              </div>
 
              <div className="wind">
-              {
-                weatherdata.wind ? <p className="bold">{weatherdata.wind.speed} MPH</p> : null
-              }
-              <p>Wind-Speed</p>
+              <p>
+                <span>Wind-Speed: </span>
+                {
+                  weatherdata.wind ? <strong>{weatherdata.wind.speed} MPH</strong> : "N/A"
+                }
+              </p>
              </div>
 
           </div> 
