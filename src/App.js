41d32efa7b44ca17e80ba952cd6f307f40a0a626
Sagar Haldar <sagarhaldar987@gmail.com>
2023-09-30 13:51:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,98 +3,155 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-
-  // const[weatherdata, setWeatherData] = useState(null);
-
-  const [location, setLocation] = useState("Mumbai");
-
-  let weatherdata = null;
-   
+  const[weatherdata, setWeatherData] = useState(null);
+  const [location, setLocation] = useState("");
   const [DataIn, setDataIn] = useState(false);
 
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f4bd0d27ccf0d983a90da29ef0130a17&units=metric`;
+
   const fetchApi = async () => {
-
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f4bd0d27ccf0d983a90da29ef0130a17&units=metric`).then((data) => {
-      weatherdata = data.data;
+    axios.get(URL)
+    .then(response => {
+      setWeatherData(response.data);
       setDataIn(true);
-      console.log(weatherdata.main);
-    }).catch(err => {
+    })
+    .catch(err => {
+      setWeatherData(null);
       setDataIn(false);
-      console.log("Ram NAAM SATYA HAI");
+      console.error(err);
     });
+  };
 
-  }
-
-  useEffect( () => {
-   fetchApi();  
+  useEffect(() => {
+    fetchApi();
   },[]);
 
   return (
-      <div>
+      <div className="container h-screen w-screen text-yellow-600">
+{/* 1. Entering the City Name to Search the Data */}
+    	  <div className="flex flex-row gap-2 justify-center">
+          <div>
+            <input
+              className="m-2 text-xl p-2 my-10 text-gray-300 bg-gray-900 rounded-md"
+              onChange={(event) => {setLocation(event.target.value)}}
+              placeholder='Enter Location'
+              type="search"
+            />
+          </div>
+          <div>
+            <button onClick={fetchApi} className="bg-blue-800 text-white px-5 py-2 mx-2 my-10 rounded-full">Search</button>
+          </div>
 
-
-{/* Entering the City Name to Search the Data */}
-    	  <div>
-          <input
-            className=" border-black w-full text-xl p-2"
-            onChange={(event) => {setLocation(event.target.value)}}
-            placeholder='Enter Location'
-            type="search"
-          />
-
-          <button onClick={fetchApi} className="bg-blue-800 text-white py-2 px-5 rounded">Search</button>
+          
 
         </div>
 
-{/* Getting the Data with Some Conditions */}
-
+{ /* 2. Getting the Data with Some Conditions */ }
       {
         !DataIn ?
-        (<p>No Data Found</p>)
+        (<div className="flex justify-center items-center">
+          <p>No Data Found</p>
+        </div>)
         :
         (
-          <div>
-            <div className="location">
-              <p>{location}</p>
+          <div className="flex flex-col">
+
+
+            <div className="flex justify-center gap-5 my-10">
+
+              <div className="location">
+                <p>
+                  <span>Location : </span>
+                  <strong>{location},{weatherdata.sys.country}</strong>
+                </p>
+              </div> 
+              <div className="temp">
+                <p>
+                  <span>Temperature : </span>
+                  {
+                    weatherdata.main ? <strong>{weatherdata.main.temp}°C</strong> : "Not Found"
+                  }
+                </p>
+              </div>
+
             </div>
-
-
-            <div className="temp">
-              {
-                weatherdata.main != null ? <h1>{weatherdata.main.temp}^F</h1> : <p>Not Found</p>
-              }
-              <h1> </h1>
-            </div>
-
-
-           <div className="description">
-            {/* {
-              weatherdata.weather ?  : null
-            } */}
-            <p>{weatherdata.weather[0].main}</p>
-           </div>
+            
 
           
-           <div> 
-             <div className="feels">
-              {
-                weatherdata.main ? <p className="bold">{weatherdata.main.feels_like}</p> : null
-              }
-              <p>Feels Like</p>
+           <div className="flex justify-evenly gap-48 mt-8">
+             <div className="min_temp">
+              <div className="grid grid-rows-2 place-items-center">
+               <p>
+                <div>
+                  <span>Minimum Temperature </span>
+                </div>
+                <div>
+                  {
+                    weatherdata.main ? <strong>{weatherdata.main.temp_min}°C</strong> : "N/A"
+                  }
+                </div>  
+               </p>
+              </div>
              </div>
 
-             <div className="humidity">
-              {
-                weatherdata.main ? <p className="bold">{weatherdata.main.humidity}%</p> : null
-              }
-              <p>Humidity</p>
+             <div className="min_temp">
+              <div className="grid grid-rows-2">
+               <p>
+                <div>
+                  <span>Minimum Temperature </span>
+                </div>
+                <div>
+                  {
+                    weatherdata.main ? <strong>{weatherdata.main.temp_min}°C</strong> : "N/A"
+                  }
+                </div>  
+               </p>
+              </div>
+             </div>
+        </div>
+
+
+
+
+        <div className="flex w-fit text-white mx-auto">
+             <div className="humidity border-r-2">
+              <p>
+                <div className="px-4">
+                  <span>Humidity</span>
+                </div>
+                <div className="px-11">
+                {
+                  weatherdata.main ? <strong>{weatherdata.main.humidity}%</strong> : "N/A"
+                }
+                </div> 
+              </p>
+             </div>
+
+             <div className="description border-r-2">
+              <p>
+                <div className="px-4">
+                  <span>Weather</span>
+                </div>
+                <div className="px-9">
+                  {
+                    weatherdata.weather[0] ? <strong>{weatherdata.weather[0].main}</strong> : "N/A"
+                  }
+                </div>
+              </p>
              </div>
 
              <div className="wind">
-              {
-                weatherdata.wind ? <p className="bold">{weatherdata.wind.speed} MPH</p> : null
-              }
-              <p>Wind-Speed</p>
+              <p>
+                <div className="px-4">
+                  <span>Wind-Speed</span>
+                </div>
+                <div className="px-7">
+                  {
+                    weatherdata.wind ? <strong>{weatherdata.wind.speed} MPH</strong> : "N/A"
+                  }
+                </div>
+                
+              </p>
              </div>
 
           </div> 
